8949f13437d9eb301ee2f246a7370296dbfbcb2c
axel-vair <contact@axelvair.fr>
2023-11-21 11:10:43 +0100

    :lipstick: navbar connect and disconnect style

--- src/pages/Home/navbarConnect.jsx
+++ src/pages/Home/navbarConnect.jsx
@@ -2,14 +2,18 @@ import './index.css'
 
 function NavbarConnect() {
     return (
-        <div class="navbar">
-            <button className="operation">
-                Ajouter des transactions
+        <div className="navbar">
+
+            <button className="operation relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Ajouter une transaction
+                </span>
             </button>
-            <button className="account">
+
+            <button className="account bg-emerald-600 hover:bg-emerald-400 text-white font-bold py-2 px-4 mx-4 rounded">
                 Mon compte
             </button>
-            <button className="graph">
+            <button className="graphbg-emerald-600 hover:bg-emerald-400 text-white font-bold py-2 px-4 mx-4 rounded">
                 Représentation graphique
             </button>
 
