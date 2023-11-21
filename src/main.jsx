32ba37d53d588dd1012b6f8b0c4dbbec523c5835
axel-vair <contact@axelvair.fr>
2023-11-21 11:33:40 +0100

    :lipstick: card for transactions display

--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <NavbarConnect/>
         <Index/>
         <FormRegister/>
-        <DisplayTransactions/>
         <AddTransactions/>
-
+        <DisplayTransactions/>
     </React.StrictMode>,
 )
--- src/pages/Home/displayTransactions.jsx
+++ src/pages/Home/displayTransactions.jsx
@@ -1,5 +1,5 @@
 import './index.css';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 function DisplayTransactions() {
     const [transactions, setTransactions] = useState([]);
@@ -19,17 +19,29 @@ function DisplayTransactions() {
             })
             .then((data) => setTransactions(data))
             .catch((error) => console.error("Erreur lors de la requête Fetch :", error));
-    }, []); // Empty dependency array means this effect will run once when the component mounts
+    }, [transactions]); // Empty dependency array means this effect will run once when the component mounts
 
     return (
-        <div>
+        <div className="flex flex-col items-center text-center mb-4 mt-4">
             {transactions.map((transactionsObj, index) => (
-                <li key={index}>
-                    <div>Title: {transactionsObj.title}</div>
-                    <div>Amount: {transactionsObj.amount}</div>
-                    <div>Date: {transactionsObj.date}</div>
-                </li>
+
+                <div
+                    key={index}
+                    className="mb-4 w-1/3 block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                    <h5
+                        className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                        {transactionsObj.title}
+                    </h5>
+                    <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                        {transactionsObj.amount} €
+                    </p>
+                    <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                        Le : {transactionsObj.date}
+                    </p>
+                </div>
+
             ))}
+
         </div>
 
     );
