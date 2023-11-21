f3a0bb696c8ba0d31d666c5d6dccbbf47a8f5d6a
axel-vair <contact@axelvair.fr>
2023-11-21 11:04:20 +0100

    :lipstick: form register style

--- src/pages/Home/formRegister.jsx
+++ src/pages/Home/formRegister.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from "react";
+import {useState} from "react";
 
 function FormRegister() {
     const [id, setId] = useState("");
@@ -13,9 +13,9 @@ function FormRegister() {
     async function save(event) {
         event.preventDefault();
         await api.post("/create", {
-          email: email,
-          password: password,
-          checkPassword: checkPassword
+            email: email,
+            password: password,
+            checkPassword: checkPassword
         });
         alert("Publisher Record Saved");
         // reset state
@@ -24,29 +24,37 @@ function FormRegister() {
         setPassword("");
         setCheckPassword("");
         load();
-      }
+    }
+
     return (
-        <div class="form">
+        <div className="form flex flex-col justify-center">
             <label>
                 Email:
                 <input type="text" name="mail"
-                value={email}
-                onChange={e => setEmail(e.target.value)} />
+                       value={email}
+                       className="block rounded-md p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-emerald-500 sm:text-sm sm:leading-6"
+                       onChange={e => setEmail(e.target.value)}/>
             </label>
             <label>
-               Mot de passe:
-                <input type="password" name="password" 
-                value={password}
-                onChange={e => setPassword(e.target.password)}/>
+                Mot de passe:
+                <input type="password" name="password"
+                       value={password}
+                       className="block rounded-md p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-emerald-500 sm:text-sm sm:leading-6"
+                       onChange={e => setPassword(e.target.password)}/>
             </label>
             <label>
-               Vérification du mot de passe:
-                <input type="password" name="checkPassword" 
-                value={checkPassword}
-                onChange={e => setCheckPassword(e.target.checkPassword)}
+                Vérification du mot de passe:
+                <input type="password" name="checkPassword"
+                       value={checkPassword}
+                       className="block rounded-md p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-emerald-500 sm:text-sm sm:leading-6"
+                       onChange={e => setCheckPassword(e.target.checkPassword)}
                 />
             </label>
-            <input type="submit" value="Soumettre" onClick={save}/>
+            <div>
+                <input type="submit" value="Soumettre"
+                       className="bg-emerald-600 hover:bg-emerald-400 text-white font-bold py-2 px-4 mx-4 rounded"
+                       onClick={save}/>
+            </div>
         </div>
 
     )
