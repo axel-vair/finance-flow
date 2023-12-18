144c3a0fb6e68c301bef13704bebe3d6cf1b0e54
axel-vair <contact@axelvair.fr>
2023-12-18 11:57:48 +0100

    add asynchron

--- src/pages/Home/displayTransactions.jsx
+++ src/pages/Home/displayTransactions.jsx
@@ -5,45 +5,24 @@ function DisplayTransactions() {
     const [transactions, setTransactions] = useState([]);
 
     useEffect(() => {
-        let isMounted = true;
-
-        const fetchData = async () => {
-            try {
-                const response = await fetch("http://localhost:9092/api/transactions", {
-                    method: 'GET',
-                    headers: {
-                        'Origin': 'http://localhost:5173',
-                    }
-                });
-
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const data = await response.json();
-
-                if (isMounted) {
-                    console.log('Data fetched successfully:', data);
-                    setTransactions(data);
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
+        fetch("http://localhost:9092/api/transactions", {
+            method: 'GET',
+            headers: {
+                'Origin': 'http://localhost:5173',
             }
-        };
+        })
+            .then(response => response.json()) // Parse the response as JSON
+            .then(data => {
+                setTransactions(data);
+            });
+    }); // Empty dependency array to run the effect only once on mount
 
-        fetchData();
-
-        return () => {
-            isMounted = false;
-        };
-    }, []);
-    
     return (
-        <div id="displayAllTransaction" className="flex flex-col items-center text-center overflow-auto m-1.5">
+        <div className="flex flex-col items-center text-center mb-4 mt-4">
             {transactions.map((transactionsObj, index) => (
                 <div
                     key={index}
-                    className="w-full rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                    className="mb-4 w-1/3 block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                     <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                         {transactionsObj.title}
                     </h5>
